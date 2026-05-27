91a190199a08341616a3d3e4c4364fdef2d65526
Gustavo de Oliveira Nascimento <gustavo.guon26@gmail.com>
2026-05-26 23:50:40 -0300

    Create Calculadora.cs

---
+++ Calculadora.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Calculadora
+{
+    static void Main()
+    {
+        int num1 = 10;
+        int num2 = 5;
+
+        Console.WriteLine("Soma: " + (num1 + num2));
+        Console.WriteLine("Subtração: " + (num1 - num2));
+        Console.WriteLine("Multiplicação: " + (num1 * num2));
+        Console.WriteLine("Divisão: " + (num1 / num2));
+    }
+}
